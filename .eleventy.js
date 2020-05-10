d71afd63c5bd3a9ceda3ea674ad42959175d0991
Ash Burgess <me@ashleyburg.es>
2020-05-10 22:14:44 +0100

    feat: complete basic 11ty blog setup.

---
+++ .eleventy.js
@@ -0,0 +1,157 @@
+const { DateTime } = require('luxon');
+const fs = require('fs');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const markdownItImsize = require('markdown-it-imsize');
+const Terser = require('terser');
+const CleanCSS = require('clean-css');
+
+var env = process.env.ELEVENTY_ENV;
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+  eleventyConfig.addPlugin(pluginNavigation);
+
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'dd LLL yyyy',
+    );
+  });
+
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+  });
+
+  eleventyConfig.addFilter('debug', (dateObj) => {
+    return JSON.stringify(dateObj);
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  eleventyConfig.addFilter('cssmin', function (code) {
+    if (env.trim() === 'dev') {
+      return code;
+    }
+
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addFilter('jsmin', function (code) {
+    if (env.trim() === 'dev') {
+      return code;
+    }
+
+    let minified = Terser.minify(code, { mangle: true, toplevel: true });
+    if (minified.error) {
+      return code;
+    }
+
+    return minified.code;
+  });
+
+  eleventyConfig.addFilter('orphanWrap', function (text) {
+    if (!text) return;
+    let splitSpace = text.split(' ');
+    let after = '';
+    if (splitSpace.length > 2) {
+      after += ' ';
+      let lastWord = splitSpace.pop();
+      let secondLastWord = splitSpace.pop();
+      after += `${secondLastWord}&nbsp;${lastWord}`;
+    }
+    return splitSpace.join(' ') + after;
+  });
+
+  eleventyConfig.addCollection('tagList', require('./src/_11ty/getTagList'));
+
+  function getPosts(collectionApi) {
+    return collectionApi.getFilteredByGlob('src/posts/*.md').reverse();
+  }
+
+  eleventyConfig.addCollection('posts', function (collection) {
+    return getPosts(collection);
+  });
+
+  eleventyConfig.addPassthroughCopy('./src/img');
+
+  /* Markdown Overrides */
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+  })
+    .use(markdownItAnchor, {
+      permalink: true,
+      permalinkClass: 'direct-link',
+      permalinkSymbol: '#',
+    })
+    .use(markdownItImsize);
+
+  eleventyConfig.setLibrary('md', markdownLibrary);
+
+  if (env.trim() !== 'dev') {
+    eleventyConfig.addTransform(
+      'htmlmin',
+      require('./src/_11ty/minify-html.js'),
+    );
+  } else {
+    // Browsersync Overrides
+    eleventyConfig.setBrowserSyncConfig({
+      callbacks: {
+        ready: function (err, browserSync) {
+          const content_404 = fs.readFileSync('_site/404.html');
+
+          browserSync.addMiddleware('*', (req, res) => {
+            // Provides the 404 content without redirect.
+            res.write(content_404);
+            res.end();
+          });
+        },
+      },
+      ui: false,
+      ghostMode: false,
+    });
+  }
+
+  return {
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about those.
+
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for link URLs (it does not affect your file structure)
+    // Best paired with the `url` filter: https://www.11ty.io/docs/filters/url/
+
+    // You can also pass this in on the command line using `--pathprefix`
+    // pathPrefix: "/",
+
+    markdownTemplateEngine: 'liquid',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    // passthroughFileCopy: true,
+
+    // These are all optional, defaults are shown:
+    dir: {
+      input: './src',
+      // includes: "_includes",
+      // data: "_data",
+      output: '_site',
+    },
+  };
+};
---
+++ src/_includes/index.client.js
@@ -0,0 +1,34 @@
+var theme = 'light';
+var toggleSwitch = document.querySelector('.theme-switch');
+
+function updateTheme() {
+  document.documentElement.setAttribute('data-theme', theme);
+  toggleSwitch.innerHTML =
+    'Switch to ' + (theme == 'light' ? 'dark' : 'light') + ' theme';
+}
+
+function detectTheme() {
+  //local storage is used to override OS theme settings
+  if (localStorage.getItem('theme')) {
+    if (localStorage.getItem('theme') == 'dark') {
+      theme = 'dark';
+    }
+  } else if (!window.matchMedia) {
+    //matchMedia method not supported
+    return false;
+  } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    //OS theme setting detected as dark
+    theme = 'dark';
+  }
+
+  updateTheme();
+}
+
+function toggleTheme() {
+  theme = theme === 'light' ? 'dark' : 'light';
+  localStorage.setItem('theme', theme);
+  updateTheme();
+}
+
+toggleSwitch.addEventListener('click', toggleTheme, false);
+detectTheme();
