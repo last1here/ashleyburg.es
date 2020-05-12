319af35f4973291c899a3cc60d5d1b80b478a93f
Ash Burgess <me@ashleyburg.es>
2020-05-12 23:50:10 +0100

    fix: handle false output path.

--- src/_11ty/minify-html.js
+++ src/_11ty/minify-html.js
@@ -1,12 +1,12 @@
 const htmlmin = require("html-minifier");
 
 module.exports = function(content, outputPath) {
-  if (outputPath.endsWith(".html")) {
+  if (outputPath && outputPath.endsWith('.html')) {
     let minified = htmlmin.minify(content, {
       useShortDoctype: true,
       removeComments: true,
       collapseWhitespace: true,
-      conservativeCollapse: true
+      conservativeCollapse: true,
     });
     return minified;
   }
